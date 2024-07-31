9b5eefc838810210aafaaab3b52b6c10f06327bd
Hala-Farhat <halafarhat446@gmail.com>
2024-07-31 01:21:28 -0300

    Add files via upload

---
+++ OperationsAndStrings.java
@@ -0,0 +1,40 @@
+package ch2;
+import java.util.Scanner;
+public class OperationsAndStrings {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int op1 = 5;
+		int op2 = -6;
+		int result;
+		
+//		result = ++op1 + op2--;
+//		
+//		System.out.println(op1);
+//		System.out.println(op2);
+//		System.out.println(result);
+		
+//		result = (op1++) - (++op2);
+//		
+//		System.out.println("op1= "+op1);
+//		System.out.println("op2= "+op2);
+//		System.out.print("result= "+result);
+		
+		
+//		result = (op1++) - (++op2); 	
+//		System.out.println("op1= "+op1);
+//		System.out.println("op2= "+op2);
+//		System.out.print("result= "+result);
+//		
+		Scanner in = new Scanner(System.in);
+		
+		String myName;
+//		myName = "He said: \"that is right\"";
+		myName = in.nextLine();
+		System.out.println(myName);
+		
+
+	}
+
+}
